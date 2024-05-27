fa2062e2ec37b5f52717050110133c686fbd66dc
MohadesehBi <mohadesehbi941@gmail.com>
2024-05-27 23:57:38 +0330

    Update main.py from github editor

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 length = int(input("Please select a length for printing triangle:"))
 
+if length < 2:
+    print("Your input is not valid! please enter a number more than 1. for example: 2,7,20,110")
+    return True
+
 for row in range(1, length + 1):
     print("*" * row)
